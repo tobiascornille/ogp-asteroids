cb063fd782fb506e75ac0542ab198bef5f1476a6
Tobias Cornille <cornilletobias@gmail.com>
2017-03-04 14:53:53 +0100

    Completed orientation attribute

--- OGP1617-Asteroids/src/asteroids/model/Ship.java
+++ OGP1617-Asteroids/src/asteroids/model/Ship.java
@@ -1,6 +1,8 @@
 
 package asteroids.model;
 import be.kuleuven.cs.som.annotate.*;
+import java.lang.Math;
+
 /**
  * 
  * @author Simon Merckx and Tobias Cornille
@@ -15,51 +17,27 @@ public class Ship {
 	// ctrl + space voor suggesties
 	
 	/**
-	 * Initialize this new spaceship with given X-position, given Y-position, given X-velocity, 
-	 * given Y-velocity and given orientation.
+	 * Initialize this new spaceship with given X coordinate of the position, given Y coordinate of the position, 
+	 * given X component of the velocity, given Y component of the velocity and given orientation.
 	 * 
-	 * @param positionX
+	 * @param x
 	 * 		  The X coordinate of the position of this new ship. 	  
-	 * @param positionY
+	 * @param y
 	 *        The Y coordinate of the position of this new ship.
-	 * @param velocityX
+	 * @param xVelocity
 	 * 		  the X component of the velocity of this new ship.
-	 * @param velocityY
+	 * @param yVelocity
 	 *        the Y component of the velocity of this new ship.
 	 * @param orientation
 	 * 		  The orientation of this new ship.
 	 * 
 	 * 
 	 */
-	public Ship (double positionX, double positionY, double velocityX, double velocityY, double orientation) {
-		
-		this.setPositionX(positionX);
-		this.setPositionY(positionY);
-		this.setVelocityX(velocityX);
-		this.setVelocityY(velocityY);
+	public Ship (double x, double y, double xVelocity, double yVelocity, double radius, double orientation) throws ModelException {
+		this.setPosition(x, y);
+		this.setVelocity(xVelocity, yVelocity);
 		this.setOrientation(orientation);
 		
-	}
-	
-	/**
-	 * @invar  The orientation of each Spaceship must be a valid orientation for any
-	 *         Spaceship.
-	 *       | isValidOrientation(getOrientation())
-	 */
-
-	/**
-	 * Initialize this new Spaceship with given orientation.
-	 * 
-	 * @param  orientation
-	 *         The orientation for this new Spaceship.
-	 * @pre    The given orientation must be a valid orientation for any Spaceship.
-	 *       | isValidOrientation(orientation)
-	 * @post   The orientation of this new Spaceship is equal to the given
-	 *         orientation.
-	 *       | new.getOrientation() == orientation
-	 */
-	public Ship(double orientation) {
-		this.setOrientation(orientation);
 	}
 	
 	/**
@@ -77,10 +55,10 @@ public class Ship {
 	 * @param  orientation
 	 *         The orientation to check.
 	 * @return 
-	 *       | result == 
+	 *       | result == (orientation >= 0) && (orientation <= Math.PI)
 	*/
 	public static boolean isValidOrientation(double orientation) {
-		return false;
+		return (orientation >= 0) && (orientation <= Math.PI);
 	}
 	
 	/**
@@ -105,11 +83,5 @@ public class Ship {
 	 * Variable registering the orientation of this Spaceship.
 	 */
 	private double orientation;
-	
-	private double positionX;
-	private double positionY;
-	private double velocityX;
-	private double velocityY;
-
 	
 }
