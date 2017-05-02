b505dce30c5d1f4f7d5e662f3b34b40637969a5f
Simon Merckx <merckxsimon@gmail.com>
2017-05-02 13:40:35 +0200

    Added part 3

--- OGP1617-Asteroids/src/asteroids/facade/Facade.java
+++ OGP1617-Asteroids/src/asteroids/facade/Facade.java
@@ -2,18 +2,23 @@ package asteroids.facade;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import asteroids.model.Asteroid;
 import asteroids.model.Bullet;
 import asteroids.model.Entity;
+import asteroids.model.Planetoid;
+import asteroids.model.Program;
 import asteroids.model.Ship;
 import asteroids.model.Size;
 import asteroids.model.Vector;
 import asteroids.model.World;
 import asteroids.part2.CollisionListener;
+import asteroids.part3.programs.IProgramFactory;
 import asteroids.util.ModelException;
 
-public class Facade implements asteroids.part2.facade.IFacade {
+public class Facade implements asteroids.part3.facade.IFacade {
 
 	@Override
 	public Ship createShip() throws ModelException {
@@ -377,6 +382,176 @@ public class Facade implements asteroids.part2.facade.IFacade {
 	@Override
 	public Set<? extends Object> getEntities(World world) throws ModelException {
 		return world.getEntities();
+	}
+
+	@Override
+	public int getNbStudentsInTeam() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Set<? extends Asteroid> getWorldAsteroids(World world) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addAsteroidToWorld(World world, Asteroid asteroid) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeAsteroidFromWorld(World world, Asteroid asteroid) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set<? extends Planetoid> getWorldPlanetoids(World world) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addPlanetoidToWorld(World world, Planetoid planetoid) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removePlanetoidFromWorld(World world, Planetoid planetoid) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Asteroid createAsteroid(double x, double y, double xVelocity, double yVelocity, double radius)
+			throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void terminateAsteroid(Asteroid asteroid) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isTerminatedAsteroid(Asteroid asteroid) throws ModelException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double[] getAsteroidPosition(Asteroid asteroid) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double[] getAsteroidVelocity(Asteroid asteroid) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getAsteroidRadius(Asteroid asteroid) throws ModelException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getAsteroidMass(Asteroid asteroid) throws ModelException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public World getAsteroidWorld(Asteroid asteroid) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Planetoid createPlanetoid(double x, double y, double xVelocity, double yVelocity, double radius,
+			double totalTraveledDistance) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void terminatePlanetoid(Planetoid planetoid) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isTerminatedPlanetoid(Planetoid planetoid) throws ModelException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double[] getPlanetoidPosition(Planetoid planetoid) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double[] getPlanetoidVelocity(Planetoid planetoid) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getPlanetoidRadius(Planetoid planetoid) throws ModelException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getPlanetoidMass(Planetoid planetoid) throws ModelException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getPlanetoidTotalTraveledDistance(Planetoid planetoid) throws ModelException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public World getPlanetoidWorld(Planetoid planetoid) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Program getShipProgram(Ship ship) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void loadProgramOnShip(Ship ship, Program program) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Object> executeProgram(Ship ship, double dt) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProgramFactory<?, ?, ?, ? extends Program> createProgramFactory() throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
---
+++ OGP1617-Asteroids/src/asteroids/programs/ProgramFactory.java
@@ -0,0 +1,248 @@
+package asteroids.programs;
+
+import java.util.List;
+
+import asteroids.model.Program;
+import asteroids.part3.programs.IProgramFactory;
+import asteroids.part3.programs.SourceLocation;
+
+public class ProgramFactory implements IProgramFactory<MyExpression, MyStatement, MyFunction, Program> {
+
+	@Override
+	public Program createProgram(List<MyFunction> functions, MyStatement main) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyFunction createFunctionDefinition(String functionName, MyStatement body, SourceLocation sourceLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyStatement createAssignmentStatement(String variableName, MyExpression value,
+			SourceLocation sourceLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyStatement createWhileStatement(MyExpression condition, MyStatement body, SourceLocation sourceLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyStatement createBreakStatement(SourceLocation sourceLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyStatement createReturnStatement(MyExpression value, SourceLocation sourceLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyStatement createIfStatement(MyExpression condition, MyStatement ifBody, MyStatement elseBody,
+			SourceLocation sourceLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyStatement createPrintStatement(MyExpression value, SourceLocation sourceLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyStatement createSequenceStatement(List<MyStatement> statements, SourceLocation sourceLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createReadVariableExpression(String variableName, SourceLocation sourceLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createReadParameterExpression(String parameterName, SourceLocation sourceLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createFunctionCallExpression(String functionName, List<MyExpression> actualArgs,
+			SourceLocation sourceLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createChangeSignExpression(MyExpression expression, SourceLocation sourceLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createNotExpression(MyExpression expression, SourceLocation sourceLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createDoubleLiteralExpression(double value, SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createNullExpression(SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createSelfExpression(SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createShipExpression(SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createAsteroidExpression(SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createPlanetoidExpression(SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createBulletExpression(SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createPlanetExpression(SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createAnyExpression(SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createGetXExpression(MyExpression e, SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createGetYExpression(MyExpression e, SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createGetVXExpression(MyExpression e, SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createGetVYExpression(MyExpression e, SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createGetRadiusExpression(MyExpression e, SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createLessThanExpression(MyExpression e1, MyExpression e2, SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createEqualityExpression(MyExpression e1, MyExpression e2, SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createAdditionExpression(MyExpression e1, MyExpression e2, SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createMultiplicationExpression(MyExpression e1, MyExpression e2, SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createSqrtExpression(MyExpression e, SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyExpression createGetDirectionExpression(SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyStatement createThrustOnStatement(SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyStatement createThrustOffStatement(SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyStatement createFireStatement(SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyStatement createTurnStatement(MyExpression angle, SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyStatement createSkipStatement(SourceLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
