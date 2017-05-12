722ce66e53aec63524de09b66f7bbe8724e88095
Tobias <tobiascornille@hotmail.com>
2017-05-12 20:37:02 +0200

    Fixed infinite size

--- OGP1617-Asteroids/src/asteroids/model/MinorPlanet.java
+++ OGP1617-Asteroids/src/asteroids/model/MinorPlanet.java
@@ -7,7 +7,20 @@ import be.kuleuven.cs.som.annotate.Raw;
  * A class of minor planets involving a position, velocity, radius.
  */
 public class MinorPlanet extends Entity {
+    public MinorPlanet(double radius) throws IllegalArgumentException {
+        super(radius);
+    }
 
- 
+    @Override
+    public boolean checkOverlapInWorld(World world) {
+        return false;
+    }
+
+    @Override
+    public void terminate() {
+
+    }
+
+
     //TODO: implement this shit
 } 
